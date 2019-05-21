dfa5d341d2853efde6ded98edc701406b93234ae
Karyna Avramenko <karinmia99avr@gmail.com>
2019-05-22 00:01:43 +0300

    Add django-crispy modal form for creating task

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from bootstrap_modal_forms.generic import (BSModalCreateView, BSModalDeleteView)
+
 from .forms import TaskForm
-from bootstrap_modal_forms.generic import (BSModalCreateView,
-                                           BSModalDeleteView)
 from .models import Task
 
 
@@ -12,9 +14,15 @@ def task_list(request):
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
 
-class TaskCreateView(BSModalCreateView):
-    template_name = 'tasks/create_task.html'
-    form_class = TaskForm
+# class TaskCreateView(BSModalCreateView):
+#     template_name = 'tasks/create_task.html'
+#     form_class = TaskForm
+#     success_url = reverse_lazy(task_list)
+
+class TaskUpdateView(CreateView):
+    model = Task
+    fields = ('title', 'author', 'description')
+    template_name = 'tasks/update_task.html'
     success_url = reverse_lazy(task_list)
 
 
